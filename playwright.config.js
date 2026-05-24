bcb2e79b6bde66c09f75024ede9f7994028a9ee3
Welington Steinmetz <welington.melo1@gmail.com>
2026-05-24 13:14:40 -0300

    Inclusão do dorny/test-reporter

--- playwright.config.js
+++ playwright.config.js
@@ -12,23 +12,35 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './e2e',
+
   /* Run tests in files in parallel */
   fullyParallel: true,
+
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
+
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
+
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html', { open: false }], ['junit', { outputFile: `results.xml` }]],
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+
+  /* Reporter to use */
+  reporter: [
+    ['html', { open: false }],
+    ['list'],
+    ['junit', { outputFile: 'test-results/results.xml' }]
+  ],
+
+  /* Shared settings for all the projects below. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
+
+    /* Base URL */
     baseURL: 'https://pgats-ci-example.netlify.app',
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    /* Collect trace when retrying the failed test */
     trace: 'on-first-retry',
+
     screenshot: 'only-on-failure'
   },
 
@@ -44,6 +56,7 @@ export default defineConfig({
     //   name: 'Mobile Chrome',
     //   use: { ...devices['Pixel 5'] },
     // },
+
     // {
     //   name: 'Mobile Safari',
     //   use: { ...devices['iPhone 12'] },
@@ -54,6 +67,7 @@ export default defineConfig({
     //   name: 'Microsoft Edge',
     //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
     // },
+
     // {
     //   name: 'Google Chrome',
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
